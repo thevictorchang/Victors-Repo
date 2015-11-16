a8bbe93dd09cdb8f3538333f8bca6a29f334d892
Victor Chang <thevictorchang@gmail.com>
2015-11-17 11:31:56 +1300

    Added a "Hello world" message

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
         }
     }
 }
